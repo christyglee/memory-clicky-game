066d5eff5839a0873d43fea08313e9b9a0be0352
unknown <christy.g.lee@gmail.com>
2020-11-11 22:46:52 -0800

    updated to className for spans & styling

--- memorygameapp/src/components/Navbar/index.js
+++ memorygameapp/src/components/Navbar/index.js
@@ -5,9 +5,9 @@ function Navbar(props) {
     return (
         <nav className="navbar fixed-top">
             <div className="container">
-                <span class="navbar-brand mb-0 h1">{props.title}</span>
-                <span class="navbar-brand mb-0 h1">{props.message}</span>
-                <span class="navbar-brand mb-0 h1">Score: {props.score} Best Score: {props.bestScore}</span>
+                <span className="navbar-brand mb-0 h1">{props.title}</span>
+                <span className="navbar-brand mb-0 h1">{props.message}</span>
+                <span className="navbar-brand mb-0 h1">Score: {props.score} Best Score: {props.bestScore}</span>
 
   </div>
 </nav >
